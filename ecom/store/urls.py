15f8ae0ea3deabcdaeddc547f7bfc193a0c11013
Abdullah <abdullaharshed956@gmail.com>
2024-05-02 14:21:10 +0500

    login logout functionality

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     #path/from hwere to load that/name of the function
     #jsut like the webp in laravel
     path('about/',views.about,name='about'),
+    # for each page u need template  view and url 
+    path('login/',views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout'),
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 def home(request) :
     products = Product.objects.all()
     return render(request,'home.html',{'products':products})
@@ -7,3 +9,21 @@ def home(request) :
     #just like the controllers fucntions in laravel
 def about(request) :
     return render(request,'about.html',{})
+def login_user(request) :
+    if request.method=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username,password=password)
+        if user :
+            login(request,user)
+            messages.success(request,("You have been Logged in!"))
+            return redirect('home')
+        else:
+            messages.success(request,("Error Try Again!"))
+            return redirect('login')
+    else :
+        return render(request,'login.html',{})
+def logout_user(request) :
+    logout(request)
+    messages.success(request,("You have been Logged out!"))
+    return redirect('home')
