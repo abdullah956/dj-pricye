ba36f84968f061114767763c1c192dbde39dd33f
Abdullah <abdullaharshed956@gmail.com>
2024-05-07 10:52:06 +0500

    password change

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import SignUpForm ,UpdateUserForm
+from .forms import SignUpForm ,UpdateUserForm, ChangePasswordForm
 
 def home(request) :
     products = Product.objects.all()
@@ -90,5 +90,28 @@ def update_user(request):
 	else:
 		messages.success(request, "You Must Be Logged In To Access That Page.")
 		return redirect('home')
+	
 
+def update_password(request):
+	if request.user.is_authenticated:
+		current_user = request.user
+		# Did they fill out the form
+		if request.method  == 'POST':
+			form = ChangePasswordForm(current_user, request.POST)
+			# Is the form valid
+			if form.is_valid():
+				form.save()
+				messages.success(request, "Your Password Has Been Updated.")
+				login(request, current_user)
+				return redirect('update_user')
+			else:
+				for error in list(form.errors.values()):
+					messages.error(request, error)
+					return redirect('update_password')
+		else:
+			form = ChangePasswordForm(current_user)
+			return render(request, "update_password.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page.")
+		return redirect('home')
 	
