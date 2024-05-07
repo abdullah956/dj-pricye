7e3710ad309f81dae5cc031fb57e4bd1d8041e2e
Abdullah <abdullaharshed956@gmail.com>
2024-05-07 10:26:16 +0500

    user profile update

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import SignUpForm
+from .forms import SignUpForm ,UpdateUserForm
 
 def home(request) :
     products = Product.objects.all()
@@ -73,4 +73,22 @@ def category(request, foo):
 
 def category_summary(request):
 	categories = Category.objects.all()
-	return render(request, 'category_summary.html', {"categories":categories})	
+	return render(request, 'category_summary.html', {"categories":categories})
+
+def update_user(request):	
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+		if user_form.is_valid():
+			user_form.save()
+
+			login(request, current_user)
+			messages.success(request, "User Has Been Updated.")
+			return redirect('home')
+		return render(request, "update_user.html", {'user_form':user_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page.")
+		return redirect('home')
+
+	
