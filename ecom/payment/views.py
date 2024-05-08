cf98ab2cae1079e8632a32fd94b9a41764ccaac2
Abdullah <abdullaharshed956@gmail.com>
2024-05-08 18:58:29 +0500

    Billing info setup

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm , PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
 
 def payment_success(request):
 	return render(request, "payment/checkout.html", {})
@@ -26,3 +27,27 @@ def checkout(request):
 		# Checkout as guest
 		shipping_form = ShippingForm(request.POST or None)
 		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+	
+
+def billing_info(request):
+	if request.POST:
+		# Get the cart
+		cart = Cart(request)
+		cart_products = cart.get_prods
+		quantities = cart.get_quants
+		totals = cart.cart_total()
+		#shipping_form = request.POST this is just taking the previous form and sending it to next page
+		#check if the user is logged in
+		if request.user.is_authenticated :
+			#Get the billing form 
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+		else :
+			#not logged in
+			#Get the billing form 
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+	else:
+		messages.success(request, "Access Denied")
+		return redirect('home')
+	
