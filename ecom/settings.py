de7e62bd2bc8928ead394a512ba176167e9c4429
abdullah956 <abdullaharshed956@gmail.com>
2024-08-20 01:23:00 +0500

    added middleware

--- ecom/settings.py
+++ ecom/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ecom.middleware.AdminOnlyMiddleware', 
 ]
 
 ROOT_URLCONF = 'ecom.urls'
