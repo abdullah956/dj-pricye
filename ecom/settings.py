06707f62d1f63a6e602d814e7d1520dbca7479ae
abdullah956 <abdullaharshed956@gmail.com>
2024-08-29 12:48:48 +0500

    req update

--- ecom/settings.py
+++ ecom/settings.py
@@ -72,14 +72,21 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testing',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',  
-        'PORT': '5432', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'testing',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345',
+#         'HOST': 'localhost',  
+#         'PORT': '5432', 
+#     }
+# }
 
 
 
